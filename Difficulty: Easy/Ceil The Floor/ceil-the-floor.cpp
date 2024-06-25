d996ad5142f59d08d619672254093e18b80a91f8
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-06-25 17:01:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
@@ -24,53 +24,60 @@ int main() {
 
 // } Driver Code Ends
 
-int fun2(int arr[],int n,int x)
+
+int fun1(int arr[],int low,int high,int x)
 {
-    int low=0,high=n-1;
     int ans=-1;
     while(low<=high)
     {
         int mid=(low+high)/2;
-        if(arr[mid]>=x)
-        {
-            high=mid-1;
-            ans=arr[mid];
-        }
-        else low=mid+1;
-    }
-    return ans;
-}
-
-
-
-
-int fun1(int arr[],int n,int x)
-{
-    int low=0,high=n-1;
-    int ans=-1;
-    while(low<=high)
-    {
-         int mid=(low+high)/2;
         if(arr[mid]<=x)
         {
             ans=arr[mid];
             low=mid+1;
-            
         }
-        else
-        {
-            high=mid-1;
-        }
+        else high=mid-1;
     }
     return ans;
+    
 }
+
+
+
+int fun2(int arr[],int low,int high,int x)
+{
+    int ans=-1;
+     while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(arr[mid]>=x)
+        {
+            ans=arr[mid];
+            high=mid-1;
+           
+        }
+        else  low=mid+1;
+        
+    }
+    return ans;
+    
+    
+}
+
+
+
+
 
 
 
 pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
     // code here
     sort(arr,arr+n);
-    int lb=fun1(arr,n,x);
-    int ub=fun2(arr,n,x);
-    return {lb,ub};
+    int first=fun1(arr,0,n-1,x);
+    
+    int last=fun2(arr,0,n-1,x);
+    return {first,last};
+    
+    
 }
+
