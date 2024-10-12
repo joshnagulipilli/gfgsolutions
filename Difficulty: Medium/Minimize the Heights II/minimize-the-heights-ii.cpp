e36fe590e44893ec118cbf1d799b86edddb86af5
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-10-12 10:46:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -13,15 +13,15 @@ class Solution {
     int getMinDiff(vector<int> &arr, int k) {
         // code here
         int n=arr.size();
-        if(n==1) return 0;
         sort(arr.begin(),arr.end());
         int ans=arr[n-1]-arr[0];
-        for(int i=1;i<n;i++)
+        if(n==1) return 0;
+        for(int i=0;i<n;i++)
         {
             if(arr[i]-k < 0) continue;
             int maxi=max(arr[n-1]-k,arr[i-1]+k);
-            int mini=min(arr[0]+k,arr[i]-k);
-            ans=min(ans,maxi-mini);
+            int mini= min(arr[0]+k,arr[i]-k);
+            ans=min(ans,(maxi-mini));
         }
         return ans;
     }
